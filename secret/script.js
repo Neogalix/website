ded678bb1a1a2cdc2f72e59ac8ead4642c89645a
Otocolobus <vdovin.gs@gmail.com>
2024-04-20 20:50:27 +0300

    new3

---
+++ secret/script.js
@@ -0,0 +1,9 @@
+let input = document.getElementById('input')
+let text2 = document.getElementById('text2')
+function conf() {
+    if (input.value == '1488') {
+        location.href = 'secret_main/index.html'
+    } else {
+        text2.textContent = 'Пароль неверный!'
+    }
+}
