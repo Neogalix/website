fadc13ed967a56e0d8587abd827ecd1c29c00b7b
Otocolobus <vdovin.gs@gmail.com>
2024-04-26 21:32:38 +0300

    new5

--- secret/script.js
+++ secret/script.js
@@ -1,7 +1,7 @@
 let input = document.getElementById('input')
 let text2 = document.getElementById('text2')
 function conf() {
-    if (input.value == '1HI4TL8ER8') {
+    if (input.value == 'Бебра') {
         location.href = 'secret_main/index.html'
     } else {
         text2.textContent = 'Пароль неверный!'
