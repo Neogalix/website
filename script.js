2951fd308ef4c2ed6a0addbae82838de66a60e6c
Otocolobus <vdovin.gs@gmail.com>
2024-07-05 16:57:18 +0300

    styles update

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ function vip() {
 }
 let a = 0
 let vipBtn = document.getElementById('vip-btn')
+let images = document.getElementsByClassName('hamster-img')
 function clickImg() {
     a += 1
-    if (a == 10) {
+    if (a >= 10) {
         vipBtn.style.display = 'block'
+    }
+    if (a >= 1488) {
+        for (const img of images) {
+            img.src='img/svaston.png'
+        }
     }
 }
