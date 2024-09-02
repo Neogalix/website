bd1f6e3fbf5eaf4454ee8b1a326503014abd2c1d
Otocolobus <vdovin.gs@gmail.com>
2024-09-02 16:41:17 +0300

    crocus

--- secret/password/script.js
+++ secret/password/script.js
@@ -1,7 +1,7 @@
 let input = document.getElementById('input')
 let text2 = document.getElementById('text2')
 function conf() {
-    if (input.value == 'Я не знаю') {
+    if (input.value == 'Крокус') {
         location.href = '../vip/index.html'
     } else {
         text2.textContent = 'Пароль неверный!'
