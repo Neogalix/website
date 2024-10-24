56b1fe05417270987dde29930acf3c787bf52c0a
Otocolobus <vdovin.gs@gmail.com>
2024-10-24 21:47:04 +0300

    halloween update

--- secret/password/script.js
+++ secret/password/script.js
@@ -1,4 +1,4 @@
-let password = 'ьъыlьъxыъьъьыъъpыъыьььыuъыьыbьъыьiъъ'
+let password = 'ъы2iъ1ъbыъьььыъmьъ1ьъmьыьъ3ыьpъьы1ъъx2ь1ьъ31ьыьf1ьъ2331fъыьыьъ1ьoы1ъь'
 let input = document.getElementById('input')
 let text2 = document.getElementById('text2')
 let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
