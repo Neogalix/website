a3b34e7fadc45e3a2f212886cd8f2b97435935bb
Otocolobus <vdovin.gs@gmail.com>
2024-12-02 22:17:00 +0300

    update 1.2.1

--- secret/password/script.js
+++ secret/password/script.js
@@ -1,12 +1,13 @@
-let password = 'ъы2iъ1ъbыъьььыъmьъ1ьъmьыьъ3ыьpъьы1ъъx2ь1ьъ31ьыьf1ьъ2331fъыьыьъ1ьoы1ъь'
+let password = 'фтчdяясiсъмбюцйьsуавиjрсчтфtчфзччылсыuвтйъяюсnьугимиbчлсвмзвачъtчлдьчя'
 let input = document.getElementById('input')
 let text2 = document.getElementById('text2')
-let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let engAlphabet = 'abcdefghijklmnopqrstuvwxyz'
+let rusAlphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 let endPassword = ''
 for (let i=0; i < password.length; i++) {
-    if (['1', '2', '3', 'ь', 'ы', 'ъ'].includes(password[i]) == false) {
+    if (rusAlphabet.includes(password[i]) == false) {
         if (password[i] != 'a') {
-            endPassword += alphabet[alphabet.indexOf(password[i]) - 1]
+            endPassword += engAlphabet[engAlphabet.indexOf(password[i]) - 1]
         } else {
             endPassword += 'z'
         }
